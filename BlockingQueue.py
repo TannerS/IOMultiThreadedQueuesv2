6680dbd52a4247b749851f060162bc19499a0e07
Tanner Summers <Tgsummers@cpp.edu>
2017-01-29 23:44:19 -0800

    Initial commit

---
+++ BlockingQueue.py
@@ -0,0 +1,37 @@
+# https://docs.python.org/3/library/asyncio-sync.html
+from threading import Lock, Semaphore
+
+class BlockingQueue:
+    queue = None
+    mutex = None
+    empty = None
+    full = None
+    name = None
+
+    def __init__(self, name, max = 100):
+        self.queue = []
+        self.empty = Semaphore(value=max)
+        self.full = Semaphore(0)
+        self.mutex = Lock()
+        # self.mutex = Semaphore(1) #mutex lock
+        self.name = name
+
+    def enqueue(self, data):
+        self.empty.acquire(blocking=True)
+        self.mutex.acquire(blocking=True)
+        self.queue.append(data)
+        self.mutex.release()
+        self.full.release()
+
+    def dequeue(self):
+        while True:
+            self.full.acquire(blocking=True)
+            self.mutex.acquire(blocking=True)
+            item = self.queue.pop(0)
+            self.mutex.release()
+            self.empty.release()
+            return item
+
+
+
+
